94f44c3df103a2b2b38344f3085ed552b2ddb09f
José Murillo <jooseemurillo@gmail.com>
2021-02-20 23:01:21 -0300

    patter mach using cond

--- ex_started/lib/ex_started.ex
+++ ex_started/lib/ex_started.ex
@@ -1,15 +1,16 @@
 defmodule ExStarted do
-
-  def list do
-    [1, "Murilo", :false]
+  @moduledoc """
+    This module is a example to using patter match
+  """
+  def options(op) when is_binary(op) do
+    cond do
+      op === "sum" -> fn x, y -> x + y end
+      op === "subtract" -> fn x, y -> x - y end
+      true -> {:error, "Option Invalid only input 'sum' or 'suubtract'"}
+    end
   end
 
-  def tuple do
-    {1, "Murillo", :false}
-  end
-
-  def show_index_tuple(tuple, index) do
-    tuple
-    |> elem(index)
+  def options(_op) do
+    {:error, "Type Invalid only type the string arg"}
   end
 end
